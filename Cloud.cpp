934df46d18b408e9b75a461361aa35ac19a54b80
Andy Jiang <weiwenj@andrew.cmu.edu>
2024-12-10 11:57:36 -0500

    some more fixes to the shader

--- Cloud.cpp
+++ Cloud.cpp
@@ -54,102 +54,100 @@ namespace Cloud {
     NVDF load_cloud(RTG &rtg, std::string directory)
     // assuming 64 layers, file names is either field_data.number.tga or modeling_data.number.tga
     {
-        NVDF cloud_nvdf;
-        std::string cloud_paths[2] = {std::string(directory + "field_data."), std::string(directory + "modeling_data.")};
+       NVDF cloud_nvdf;
+        std::string cloud_paths[2] = {
+            directory + "field_data.",
+            directory + "modeling_data."
+        };
 
-        { // field data
-            Helpers::AllocatedImage3D &field_data_image = cloud_nvdf.field_data;
-            const std::string& cloud_path = data_path(std::string(directory + "field_data."));
-            std::vector<unsigned char*> images(cloud_voxel_layers);
+        auto load_image_stack = [&](const std::string &path_prefix, Helpers::AllocatedImage3D &image_output, VkFormat format, bool use_float) {
             uint32_t width = 0, height = 0;
+            std::vector<void*> images(cloud_voxel_layers); // Use void* to store either float* or unsigned char*
             for (uint16_t i = 0; i < cloud_voxel_layers; ++i) {
                 char buffer[4];
-                _itoa_s(i+1, buffer, 10);
+                _itoa_s(i + 1, buffer, 10);
                 std::string number(buffer);
                 while (number.length() < 3) {
                     number = '0' + number;
                 }
-                
+
                 int x, y, comp;
-                images[i] = static_cast<unsigned char*>(stbi_load((cloud_path + number + ".tga").c_str(), &x, &y, &comp, 0));
-                assert(images[i] != NULL);
-                assert(comp == 4);
+                if (use_float) {
+                    images[i] = stbi_loadf((path_prefix + number + ".tga").c_str(), &x, &y, &comp, 0);
+                } else {
+                    images[i] = stbi_load((path_prefix + number + ".tga").c_str(), &x, &y, &comp, 0);
+                }
+
+                assert(images[i] != nullptr); // Ensure the image is loaded
+                assert(comp == 4);           // Ensure RGBA format
                 if (width == 0 && height == 0) {
                     width = x;
                     height = y;
                 }
                 assert(uint32_t(x) == width);
                 assert(uint32_t(y) == height);
-                
             }
-            uint32_t image_size = width * height * 4;
-            uint32_t total_image_size = image_size * cloud_voxel_layers;
-            std::vector<unsigned char> merged_images(total_image_size);
-            for (size_t i = 0; i < cloud_voxel_layers; ++i) {
-                std::memcpy(merged_images.data() + i * image_size, images[i], image_size);
-            }
-            
-            field_data_image = rtg.helpers.create_image_3D(
-                VkExtent3D{ .width = uint32_t(width), .height = uint32_t(height), .depth = uint32_t(cloud_voxel_layers)  },
-                VK_FORMAT_R8G8B8A8_UNORM,
-                VK_IMAGE_TILING_OPTIMAL,
-                VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT,
-                VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
-                Helpers::Unmapped
-            );
-            rtg.helpers.transfer_to_image_3D(merged_images.data(), total_image_size, field_data_image);
-            
-            for (unsigned char* image : images) {
-                stbi_image_free(image);
-            }
-        }
 
-        { // model data
-            Helpers::AllocatedImage3D &model_data_image = cloud_nvdf.modeling_data;
-            const std::string& cloud_path = data_path(std::string(directory + "modeling_data."));
-            std::vector<unsigned char*> images(cloud_voxel_layers);
-            uint32_t width = 0, height = 0;
-            for (uint16_t i = 0; i < cloud_voxel_layers; ++i) {
-                char buffer[4];
-                _itoa_s(i+1, buffer, 10);
-                std::string number(buffer);
-                while (number.length() < 3) {
-                    number = '0' + number;
-                }
-                
-                int x, y, comp;
-                images[i] = static_cast<unsigned char*>(stbi_load((cloud_path + number + ".tga").c_str(), &x, &y, &comp, 0));
-                assert(images[i] != NULL);
-                assert(comp == 4);
-                if (width == 0 && height == 0) {
-                    width = x;
-                    height = y;
-                }
-                assert(uint32_t(x) == width);
-                assert(uint32_t(y) == height);
-                
-            }
             uint32_t image_size = width * height * 4;
             uint32_t total_image_size = image_size * cloud_voxel_layers;
-            std::vector<unsigned char> merged_images(total_image_size);
-            for (size_t i = 0; i < cloud_voxel_layers; ++i) {
-                std::memcpy(merged_images.data() + i * image_size, images[i], image_size);
+
+            if (use_float) {
+                std::vector<float> merged_images(total_image_size);
+                for (size_t i = 0; i < cloud_voxel_layers; ++i) {
+                    std::memcpy(merged_images.data() + i * image_size, images[i], image_size * sizeof(float));
+                }
+
+                image_output = rtg.helpers.create_image_3D(
+                    VkExtent3D{width, height, uint32_t(cloud_voxel_layers)},
+                    format,
+                    VK_IMAGE_TILING_OPTIMAL,
+                    VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT,
+                    VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
+                    Helpers::Unmapped
+                );
+
+                rtg.helpers.transfer_to_image_3D(merged_images.data(), merged_images.size() * sizeof(float), image_output);
+            } else {
+                std::vector<unsigned char> merged_images(total_image_size);
+                for (size_t i = 0; i < cloud_voxel_layers; ++i) {
+                    std::memcpy(merged_images.data() + i * image_size, images[i], image_size);
+                }
+
+                image_output = rtg.helpers.create_image_3D(
+                    VkExtent3D{width, height, uint32_t(cloud_voxel_layers)},
+                    format,
+                    VK_IMAGE_TILING_OPTIMAL,
+                    VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT,
+                    VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
+                    Helpers::Unmapped
+                );
+
+                rtg.helpers.transfer_to_image_3D(merged_images.data(), merged_images.size(), image_output);
             }
-            
-            model_data_image = rtg.helpers.create_image_3D(
-                VkExtent3D{ .width = uint32_t(width), .height = uint32_t(height), .depth = uint32_t(cloud_voxel_layers)  },
-                VK_FORMAT_R8G8B8A8_UNORM,
-                VK_IMAGE_TILING_OPTIMAL,
-                VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT,
-                VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
-                Helpers::Unmapped
-            );
-            rtg.helpers.transfer_to_image_3D(merged_images.data(), total_image_size, model_data_image);
-            
-            for (unsigned char* image : images) {
-                stbi_image_free(image);
+
+            for (void* image : images) {
+                stbi_image_free(image); // Free loaded images
             }
-        }
+        };
+        
+        // Load field data
+        load_image_stack(
+            directory + "field_data.",
+            cloud_nvdf.field_data,
+            VK_FORMAT_R32G32B32A32_SFLOAT,
+            true
+        );
+
+        // Load modeling data
+        load_image_stack(
+            directory + "modeling_data.",
+            cloud_nvdf.modeling_data,
+            VK_FORMAT_R8G8B8A8_UNORM,
+            false
+        );
+
+
+
         
         return cloud_nvdf;
     }
